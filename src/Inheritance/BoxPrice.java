ead4acf53cf8f8eadfcc851738c6561fc60c62f7
Anmol Verma <anmolverma075@gmail.com>
2023-10-08 21:00:40 +0530

    OOPS !!!!

---
+++ src/Inheritance/BoxPrice.java
@@ -0,0 +1,24 @@
+package Inheritance;
+
+public class BoxPrice extends BoxWeight{
+    double price;
+
+    BoxPrice(){
+        super();
+        this.price = -1;
+    }
+
+    BoxPrice(BoxPrice other){
+        super(other);
+        this.price = other.price;
+    }
+    public BoxPrice(double l , double h , double w , double weight , double cost){
+        super(l ,h , w , weight);
+        this.price = price;
+    }
+
+    public BoxPrice(double side , double weight , double price){
+        super(side ,weight);
+        this.price = price;
+    }
+}
---
+++ src/Inheritance/BoxWeight.java
@@ -0,0 +1,29 @@
+package Inheritance;
+
+public class BoxWeight extends Box {
+    double weight;
+
+    public BoxWeight(){
+        this.weight = -1;
+    }
+
+    BoxWeight (BoxWeight other){
+        super(other);
+        weight = other.weight;
+    }
+    //above statement is same as  "Box boxx = new BoxWeight(12, 11 , 15 , 15);"
+
+    public BoxWeight(double l , double w , double h , double weight){
+        super(l , h , w);
+
+        //suppose weight is also present in parent class & we explicitly want to access the weight of it so we use super instead of this.
+        //we would use "this" to enter weight of child class.
+        System.out.println(super.weight);
+        this.weight = weight;
+    }
+
+    public BoxWeight(double side , double weight){
+        super(side);
+        this.weight = weight;
+    }
+}
